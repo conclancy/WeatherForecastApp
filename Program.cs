8c7f662435c6ae51509272657155cd4038f6814f
conclancy <conclancy@hotmail.com>
2020-12-02 23:40:55 -0500

    Added Location Information API

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WeatherForecastLibrary;
 
 namespace WeatherForecastApp
 {
     public class Program
     {
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Starting...");
@@ -15,10 +21,39 @@ namespace WeatherForecastApp
             var secretProvider = config.Providers.First();
             if (!secretProvider.TryGet("ApiKey", out var secretPass)) return;
 
-            Console.WriteLine($"Secret passcode: {secretPass}");
+            Location l = new Location();
+            l.LocationAsync(secretPass).Wait();
+
+            Console.WriteLine($"Coordinates: {l.Cor}");
+        }
+    }
+
+    public class Location 
+    {
+        private IHttpClientFactory _clientFactory;
+
+        public float[] Cor { get; private set; }
+
+        public async Task LocationAsync(string secrets)
+        {
+            LocationModel location;
+            string errorString;
+
+            string locationUrl = $"http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&postalCode=02155&key={secrets}";
+            var locationRequest = new HttpRequestMessage(HttpMethod.Get, locationUrl);
+            var locationClient = _clientFactory.CreateClient();
+
+            HttpResponseMessage locationResponse = await locationClient.SendAsync(locationRequest);
+
+            location = await locationResponse.Content.ReadFromJsonAsync<LocationModel>();
+
+            foreach (Resourceset rs in location.resourceSets)
+            {
+                foreach (Resource r in rs.resources)
+                {
+                    Cor = r.bbox;
+                }
+            }
         }
     }
 }
-
-
-http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&postalCode=02155&key={AoFEY3O5nYf9OveRWHxUwZGsiA29KH9pKiCjILV0RdVbecU_qtjws1hDYZ_wSdvT}
