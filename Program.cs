50cef03f9072d9370b77c4437d2f00adf9c0486f
conclancy <conclancy@hotmail.com>
2020-12-02 22:25:52 -0500

    Secrets.json logic created.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
 
 namespace WeatherForecastApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            // Get secrets from secrets.json 
+            var secretProvider = config.Providers.First();
+            if (!secretProvider.TryGet("ApiKey", out var secretPass)) return;
+
+            Console.WriteLine($"Secret passcode: {secretPass}");
         }
     }
 }
+
+
+http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&postalCode=02155&key={AoFEY3O5nYf9OveRWHxUwZGsiA29KH9pKiCjILV0RdVbecU_qtjws1hDYZ_wSdvT}
